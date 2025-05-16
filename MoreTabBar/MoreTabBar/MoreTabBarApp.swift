a3392d9b31270cff688e51ad9167f51d6a22180b
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 03:47:50 +0700

    more tab bar

---
+++ MoreTabBar/MoreTabBar/MoreTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoreTabBarApp.swift
+//  MoreTabBar
+//
+//  Created by Adrian Suryo Abiyoga on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MoreTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
